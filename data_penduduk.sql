e9b51c18a311e2e0ec96d7a10bcdfa025796ac16
Cintiaagian <cintiaagian2000@gmail.com>
2025-11-11 20:27:04 +0700

    addmysql

---
+++ data_penduduk.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2025 at 02:22 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts_pemrogramanweb_1202305010`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_penduduk`
+--
+
+CREATE TABLE `data_penduduk` (
+  `id_penduduk` int(25) DEFAULT NULL,
+  `Nama` varchar(60) NOT NULL,
+  `Umur` int(2) NOT NULL,
+  `Jenis_Kelamin` varchar(10) NOT NULL,
+  `Alamat` varchar(70) NOT NULL,
+  `Status_Pernikahan` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
